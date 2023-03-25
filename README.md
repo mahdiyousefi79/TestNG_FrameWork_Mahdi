d6876748bb6c7cf9d5f71b83bdde8333a9ed3b81
mahdi <mahdiybo@gmail.com>
2023-03-25 10:59:00 -0700

    Initial new TestNG project

---
+++ README.md
@@ -0,0 +1,25 @@
+# TestNG framework created by Mahdi Yousefi
+
+
+## Similar to Amazon framework
+
+### BasePage Class
+#### Some of the commonly used methods in a BasePage class in TestNG include:
+
+* Initialization of the driver object for web automation frameworks
+* Navigation to the page URL
+* Verification of page title and URL
+* Handling of pop-ups and alerts
+* Common actions such as click, send keys, and select option
+* Waiting for the page to load and element to be visible using explicit and implicit waits.
+* The purpose of the BasePage class is to provide a common set of methods that can be inherited by other page classes. This reduces the amount of code duplication and helps to maintain a consistent structure across the test automation framework.
+
+### Maven Dependency
+* will complete later
+
+
+
+### TestBase class
+*   Initialization of the WebDriver instance
+*   Teardown test
+
--- src/test/java/com/vytrack/pages/BasePage.java
+++ src/test/java/com/vytrack/pages/BasePage.java
@@ -13,10 +13,40 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class BasePage {
 
-// In this page we use for GateWay page also we write constructor here, then no need to create constructor for each class
+    // In this page we use for GateWay page also we write constructor here, then no need to create constructor for each class
     public BasePage() {
         PageFactory.initElements(Driver.get(), this);
     }
 
+    @FindBy(css = "div[class='loader-mask shown']")
+    @CacheLookup
+    protected WebElement loaderMask;
 
+    @FindBy(css = "h1[class='oro-subtitle']")
+    public WebElement pageSubTitle;
+
+
+    /**
+     * Waits until loader screen present. If loader screen will not pop up at all,
+     * NoSuchElementException will be handled  bu try/catch block
+     * Thus, we can continue in any case.
+     */
+    public void waitUntilLoaderScreenDisappear() {
+        try {
+            WebDriverWait wait = new WebDriverWait(Driver.get(), 5);
+            wait.until(ExpectedConditions.invisibilityOf(loaderMask));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @return page name, for example: Dashboard
+     */
+    public String getPageSubTitle() {
+        //ant time we are verifying page name, or page subtitle, loader mask appears
+        waitUntilLoaderScreenDisappear();
+//        BrowserUtils.waitForStaleElement(pageSubTitle);
+        return pageSubTitle.getText();
+    }
 }
